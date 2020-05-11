e4574b06a61c8d14ed9e09b9a1fdb1881742a8ef
Henrik Giesel <hengiesel@gmail.com>
2020-05-11 15:55:29 +0200

    Add priority queue class

--- src/filterManager/deferred.ts
+++ src/filterManager/deferred.ts
@@ -1,19 +1,27 @@
 export type Deferred = (name: string, ...rest: any[]) => void
 
+interface DeferredEntry {
+    procedure: Deferred
+    priority: number
+}
+
 export class DeferredApi {
-    private deferred: Map<string, Deferred>
+    private deferred: Map<string, DeferredEntry>
 
     constructor() {
         this.deferred = new Map()
     }
 
-    register(name: string, proc: Deferred): void {
-        this.deferred.set(name, proc)
+    register(name: string, proc: Deferred, prio=50): void {
+        this.deferred.set(name, {
+            procedure: proc,
+            priority: prio,
+        })
     }
 
-    registerIfNotExists(name: string, proc: Deferred): void {
+    registerIfNotExists(name: string, proc: Deferred, prio=50): void {
         if (!this.has(name)) {
-            this.register(name, proc)
+            this.register(name, proc, prio)
         }
     }
 
@@ -30,8 +38,8 @@ export class DeferredApi {
     }
 
     executeEach(...args: any[]): void {
-        for (const [name, func] of this.deferred) {
-            func(name, ...args)
+        for (const [name, def] of this.deferred) {
+            def.procedure(name, ...args)
         }
     }
 }
---
+++ src/filterManager/priorityQueue.ts
@@ -0,0 +1,79 @@
+const top = 0
+const parent = (i: number): number => ((i + 1) >>> 1) - 1
+const left = (i: number): number => (i << 1) + 1
+const right = (i: number): number => (i + 1) << 1
+
+type Comparator = (a: unknown, b: unknown) => boolean
+
+export class PriorityQueue {
+    private readonly _heap: unknown[]
+    private readonly _comparator: Comparator
+
+    constructor(comparator: Comparator = (a, b) => a > b) {
+        this._heap = []
+        this._comparator = comparator
+    }
+
+    private greater(i: number, j: number): boolean {
+        return this._comparator(this._heap[i], this._heap[j])
+    }
+
+    private swap(i: number, j: number): void {
+        [this._heap[i], this._heap[j]] = [this._heap[j], this._heap[i]]
+    }
+
+    private siftUp(): void {
+        let node = this.size() - 1;
+        while (node > top && this.greater(node, parent(node))) {
+            this.swap(node, parent(node))
+            node = parent(node)
+        }
+    }
+
+    private siftDown(): void {
+        let node = top
+        while (
+            (left(node) < this.size() && this.greater(left(node), node)) ||
+            (right(node) < this.size() && this.greater(right(node), node))
+        ) {
+            let maxChild = (right(node) < this.size() && this.greater(right(node), left(node))) ? right(node) : left(node)
+            this.swap(node, maxChild)
+            node = maxChild
+        }
+    }
+
+    size(): number {
+        return this._heap.length
+    }
+
+    isEmpty(): boolean {
+        return this.size() === 0
+    }
+
+    peek(): unknown {
+        return this._heap[top]
+    }
+
+    push(...values: unknown[]): number {
+        values.forEach(value => {
+            this._heap.push(value)
+            this.siftUp()
+        })
+
+        return this.size()
+    }
+
+    pop(): unknown {
+        const poppedValue = this.peek()
+        const bottom = this.size() - 1
+
+        if (bottom > top) {
+            this.swap(top, bottom)
+        }
+
+        this._heap.pop()
+        this.siftDown()
+
+        return poppedValue
+    }
+}

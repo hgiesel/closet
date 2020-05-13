8ae45b73588bde5b00c399682970102a5fe450b6
Henrik Giesel <hengiesel@gmail.com>
2020-05-13 20:22:15 +0200

    Update compile-parser script

--- bin/compile-parser.sh
+++ bin/compile-parser.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-declare DIR="$(realpath "${BASH_SOURCE%/*}")"
+# declare DIR="$(realpath "${BASH_SOURCE%/*}")"
+declare DIR="$(cd "$(dirname "$0")" && pwd -P)"
 
 if ! type nearleyc &> /dev/null; then
   echo 'No nearleyc executable found in $PATH'

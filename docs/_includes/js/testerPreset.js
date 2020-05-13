3b91142afc9a438e05c4266049eb0da46492b548
Henrik Giesel <hengiesel@gmail.com>
2020-05-13 17:58:40 +0200

    Add preset url query param

--- docs/_includes/js/testerPreset.js
+++ docs/_includes/js/testerPreset.js
@@ -45,12 +45,13 @@ const codeCM = CodeMirror.fromTextArea(codeTA, {
 
 const sparams = new URLSearchParams(location.search)
 
-if (sparams.has('txt')) {
-    codeCM.setValue(sparams.get('txt'))
+
+if (sparams.has('text')) {
+    codeCM.setValue(sparams.get('text'))
 }
-else if (codeCM.getValue.length === 0) {
-    const defaultTxt =  'This is a [[hl::sample::text]].\nReplace it with whatever you [[em::w[[opt::ish||ant]]||desire]]!'
-    codeCM.setValue(defaultTxt)
+
+if (sparams.has('preset')) {
+    codeCM.setValue(sparams.get('text'))
 }
 
 ////////////////////

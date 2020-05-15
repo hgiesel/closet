9164d22444890281d699de60a5b540cd3683d11b
Henrik Giesel <hengiesel@gmail.com>
2020-05-15 18:31:46 +0200

    Try out ankiweb action

---
+++ bin/zip.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# declare DIR="$(realpath "${BASH_SOURCE%/*}")"
+declare DIR="$(cd "$(dirname "$0")" && pwd -P)/../anki"
+
+if [[ "$1" == '-a' ]]; then
+  # for uploading to AnkiWeb
+  declare addon_id='unknown'
+else
+  # for installing myself
+  declare addon_id='anki_closet'
+fi
+
+rm -f "${DIR}/${addon_id}.ankiaddon"
+
+zip -r "${DIR}/${addon_id}.ankiaddon" \
+  "${DIR}/config."{json,md} \
+  "${DIR}/__init__.py" \

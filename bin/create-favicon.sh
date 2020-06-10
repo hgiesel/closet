00ffbd66df1c547789cabb4eefeac177bfd4eef5
Henrik Giesel <hengiesel@gmail.com>
2020-06-11 01:37:29 +0200

    Add new favicon script

--- bin/create-favicon.sh
+++ bin/create-favicon.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 declare DIR="$(cd "$(dirname "$0")/.." && pwd -P)"
 
-convert "$DIR/images/logo.png"  -background white \
-  \( -clone 0 -resize 16x16 -extent 16x16 \) \
-  \( -clone 0 -resize 32x32 -extent 32x32 \) \
-  \( -clone 0 -resize 48x48 -extent 48x48 \) \
-  \( -clone 0 -resize 64x64 -extent 64x64 \) \
-  -delete 0 -alpha off -colors 256 "$DIR/docs/favicon.ico"
+convert "$DIR/images/logo.png" -define icon:auto-resize=64,48,32,16 "$DIR/docs/favicon.ico"
 
 # copy logo to image folder"
 cp -f "$DIR/images/weblogo.png" "$DIR/docs/assets/images"

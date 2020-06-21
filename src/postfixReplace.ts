3442352d9a9d25d669b4b377d1644044d5b9a55e
Henrik Giesel <hengiesel@gmail.com>
2020-06-21 15:51:37 +0200

    Make meaning of "baseDepth" and "depth" a bit more sound

--- src/postfixReplace.ts
+++ src/postfixReplace.ts
@@ -5,9 +5,17 @@ import { calculateCoordinates, replaceAndGetOffset, removeViaBooleanList } from 
 
 // traverses in postfix order
 export const postfixReplace = (baseText: string, rootTag: TagInfo, baseDepth: number, filterProcessor: FilterProcessor): [string, number[], boolean[], [number, number][]] => {
+
     const baseStack = []
 
     const tagReduce = ([text, tagPath, stack, readyStack]: [string, number[], number[], boolean[]], tagInfo: TagInfo): [string, number[], number[], boolean[]] => {
+        /** 
+         *
+         * @param text - text with tags, is modified throughout the function
+         * @param tagPath - used for roundInfo path info, indicates location of tag as a string of numbers
+         * @param stack - the number stack for the pushdown automaton logic of the function - used to calculate left offset and inner offset
+         * @param readyStack - used to indicate whether inner tags are ready
+         */
 
         // going DOWN
         stack.push(stack[stack.length - 1])
@@ -29,13 +37,13 @@ export const postfixReplace = (baseText: string, rootTag: TagInfo, baseDepth: nu
         ] = calculateCoordinates(tagInfo.start, tagInfo.end, leftOffset, innerOffset)
 
         // correctly treat the base levels: they don't have have tags!
-        const depth = tagPath.length + 1
-        const tagData = depth <= baseDepth
+        const depth = tagPath.length
+        const tagData = depth < baseDepth
             ? tagInfo.data.shadowFromText(modText, lend, rend)
             : tagInfo.data.shadowFromTextWithoutDelimiters(modText, lend, rend)
 
-        // save base tags
-        if (depth === baseDepth) {
+        // save uppermost tags beneath base
+        if (depth === baseDepth - 1) {
             baseStack.push([lend, rend])
         }
 

4e3415546a2f00356262149aef79f7faaaf2bd2b
Henrik Giesel <hengiesel@gmail.com>
2020-08-19 18:04:32 +0200

    Fix zipping script

--- bin/zip.sh
+++ bin/zip.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 declare DIR="$(cd "$(dirname "$0")/.." && pwd -P)"
-
-if [[ ! -f  "$DIR/anki/src/web/closet.js" ]]; then
-  echo 'Refuse zipping. You need to build closet for anki first.'
-  exit -5
-fi
-
-if [[ "$1" =~ ^-?a$ ]]; then
-  # for uploading to AnkiWeb
-  declare addon_id='0'
-else
-  # for installing myself
-  declare addon_id='anki_closet'
-fi
+declare addon_id='TemplateOverload'
 
 cd "$DIR"
 
@@ -24,4 +12,5 @@ zip -r "$DIR/build/$addon_id.ankiaddon" \
   ""*".py" \
   "manifest.json" \
   "src/"*".py" \
-  "src/web/"*".js"
+  "web/"*".js" \
+  "icons/"*

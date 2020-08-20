95908cea58d3658b4cf6ea52e5b8e053694cb754
Henrik Giesel <hengiesel@gmail.com>
2020-08-20 02:27:55 +0200

    First workable generator.ts

--- src/recipes/generator.ts
+++ src/recipes/generator.ts
@@ -1,12 +1,19 @@
-import type { Registrar, TagData, Internals } from './types'
+import type { Registrar, WeakFilter, TagData, Internals } from './types'
 
 type NumberGenAlgorithm = (min: number, max: number, extra: number) => string
 
 const maxTries = 10_000
-const numberGenerator = (algorithm: NumberGenAlgorithm) => function*(min: number, max: number, extra: number, filter = false) {
-  const banDomain = []
-
+const numberGenerator = (
+    algorithm: NumberGenAlgorithm,
+) => function*(
+    min: number,
+    max: number,
+    extra: number,
+    banDomain = [],
+    filter = false,
+): Generator<string, void, unknown> {
   let tries = 0
+
   while (banDomain.length < max - min && tries < maxTries) {
     const randomValue = algorithm(min, max, extra)
 
@@ -19,29 +26,48 @@ const numberGenerator = (algorithm: NumberGenAlgorithm) => function*(min: number
   }
 }
 
-const intGenerator = numberGenerator((min, max, extra) => String((Math.floor(Math.random() * max) + min) * extra))
-const realGenerator = numberGenerator((min, max, extra) => (Math.random() * (max - min) + min).toFixed(extra))
+const intGenerator = numberGenerator(
+    (min, max, extra) => String((Math.floor(Math.random() * max) + min) * extra)
+)
 
-export const generateRecipe = ({
+const realGenerator = numberGenerator((min, max, extra) => (
+    Math.random() * (max - min) + min).toFixed(extra)
+)
+
+export const generateRecipe = <T extends {}>({
     tagname = 'gen',
     uniqueConstraintId = 'uniq',
-} = {}) => (registrar: Registrar<{}>) => {
+} = {}) => (registrar: Registrar<T>) => {
 
     const uniqConstraintPrefix = `gen:${uniqueConstraintId}`
 
-    const generateFilter = ({ values, fullOccur, num }: TagData, { memory }: Internals<{}>) => {
+    const generateFilter: WeakFilter<T> = ({ values, fullOccur, num }: TagData, { memory }: Internals<{}>) => {
         const [min = 0, max = 100, extra = 1] = values.length === 1
             ? [0, values[0], 1]
             : values
 
-        const uniqueConstraint = Number.isInteger(num)
-            ? `${uniqConstraintPrefix}:${fullOccur}:${num}`
-            : `${uniqConstraintPrefix}:${fullOccur}`
+        const uniqueConstraintId = Number.isInteger(num)
+            ? `${uniqConstraintPrefix}:${num}`
+            : uniqConstraintPrefix
 
+        const generateId = `gen:${tagname}:${fullOccur}`
 
-        const result = memory.lazy(uniqueConstraint, 0)
+        const result = memory.lazy(generateId, (): string => {
+            const gen = intGenerator(
+                min,
+                max,
+                extra,
+                Number.isInteger(num)
+                ? memory.get(uniqueConstraintId, [])
+                : [],
+            )
 
-        return { ready: true }
+            const generated = gen.next()
+
+            return generated.value || ''
+        })
+
+        return { result: result, ready: true }
     }
 
     registrar.register(tagname, generateFilter)

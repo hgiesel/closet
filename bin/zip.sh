74441f6b7af36bde961ec154b8ce2271dee590f2
Henrik Giesel <hengiesel@gmail.com>
2020-08-24 02:31:37 +0200

    Remove automatic build-anki from zip script

--- bin/zip.sh
+++ bin/zip.sh
@@ -2,10 +2,6 @@
 declare DIR="$(cd "$(dirname "$0")/.." && pwd -P)"
 declare addon_id='ClosetForAnki'
 
-cd "$DIR"
-
-npm run build-anki
-
 cd "$DIR/anki"
 
 zip -r "$DIR/build/$addon_id.ankiaddon" \

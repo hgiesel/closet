2f6ebb5a38ba8995d5a2dd7a331b4fb6bd9ac354
Henrik Giesel <hengiesel@gmail.com>
2020-09-01 16:03:30 +0200

    Add some more default setup

--- anki/web/setup.js
+++ anki/web/setup.js
@@ -1,4 +1,16 @@
 filterManager.install(
     closet.recipes.shuffle({ tagname: 'mix' }),
     closet.recipes.order({ tagname: 'ord' }),
+
+    closet.recipes.cloze.show({ tagname: 'c' }),
+    closet.recipes.cloze.reveal({ tagname: 'cr' }),
+    closet.recipes.cloze.hide({ tagname: 'ch' }),
+
+    closet.recipes.multipleChoice.show({ tagname: 'mc' }),
+    closet.recipes.multipleChoice.reveal({ tagname: 'mcr' }),
+    closet.recipes.multipleChoice.hide({ tagname: 'mch' }),
+
+    closet.browser.recipes.rect.show({ tagname: 'rect' }),
+    closet.browser.recipes.rect.reveal({ tagname: 'rectr' }),
+    closet.browser.recipes.rect.hide({ tagname: 'recth' }),
 )

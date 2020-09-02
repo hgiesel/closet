9952749c9abb6610ea84ba68da337f0c69b594bb
Henrik Giesel <hengiesel@gmail.com>
2020-09-02 12:18:37 +0200

    Put terser options on toplevel

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,21 @@
 const path = require('path')
+
 const Terser = require('terser-webpack-plugin')
+const terserOptions = {
+    mangle: true,
+    output: {
+        ecma: 6 /* default is 5 */,
+    },
+    ecma: 8, // specify one of: 5, 6, 7 or 8
+    module: false,
+    nameCache: null, // or specify a name cache object
+    keep_classnames: true,
+    keep_fnames: true,
+    ie8: false,
+    safari10: false,
+    toplevel: false,
+    warnings: false,
+}
 
 module.exports = (env, argv) => {
     const destination = env && env.destination === 'docs'
@@ -42,23 +58,7 @@ module.exports = (env, argv) => {
         optimization: {
             minimize: argv.mode === 'production' ? true : false,
             minimizer: [
-                new Terser({
-                    terserOptions: {
-                        mangle: true,
-                        output: {
-                            ecma: 6 /* default is 5 */,
-                        },
-                        ecma: 8, // specify one of: 5, 6, 7 or 8
-                        ie8: false,
-                        module: false,
-                        nameCache: null, // or specify a name cache object
-                        keep_classnames: true,
-                        keep_fnames: true,
-                        safari10: false,
-                        toplevel: false,
-                        warnings: false,
-                    }
-                }),
+                new Terser({ terserOptions }),
             ],
         },
 

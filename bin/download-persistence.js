cd7757dd4b28b4c5163b8a360c6f4da9a33f48be
Henrik Giesel <hengiesel@gmail.com>
2020-09-28 00:55:30 +0200

    Terse downloaded anki persistence

--- bin/download-persistence.js
+++ bin/download-persistence.js
@@ -1,4 +1,5 @@
 const https = require('https')
+const terser = require('terser')
 const fs = require('fs')
 
 const sourceURL = 'https://raw.githubusercontent.com/SimonLammer/anki-persistence/master/script.js'
@@ -8,10 +9,26 @@ function ab2str(buf) {
 }
 
 const filePath = '../anki/web/persistence.js'
-const file = fs.createWriteStream(filePath)
+
+const writeToFile = (code) => {
+    fs.writeFile(filePath, code, () => {
+        process.stdout.write(`Anki Persistence was successfully written to "${filePath}"`)
+    })
+}
 
 https.get(sourceURL, (res) => {
-    res.pipe(file)
+    let data = ''
+
+    res.on('data', (chunk) => {
+        data += ab2str(chunk)
+    })
+
+    res.on('end', () => {
+        terser.minify(data)
+            .then((minified) => minified.code)
+            .then(writeToFile)
+            .catch(process.stderr.write)
+    })
 }).on('error', (_err) => {
     process.stdout.write('Anki Persistence does not seem to be available anymore')
 })

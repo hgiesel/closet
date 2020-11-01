9a63c5ac554971199421aaef799d3b72a1a4ce03
Henrik Giesel <hengiesel@gmail.com>
2020-11-02 00:01:35 +0100

    Update user.js to conditionally trigger on onShownHook

--- anki/web/user.js
+++ anki/web/user.js
@@ -1,22 +1,38 @@
-function userLogic(
+const closetUserLogic = (
     closet,
     preset,
     chooseMemory,
-) {
+) => {
     $$editableCode
 }
 
+const closetInit = (closet) => {
+    try {
+        const times = closet.anki.init(closet, closetUserLogic, '$$cardType', '$$tagsFull', '$$side')
+        console.info(`Closet executed in ${times.map(t => t.toFixed(3))}ms.`)
+    }
+    catch (error) {
+        console.error('An error occured while executing Closet:', error)
+    }
+    finally {
+        closet.browser.cleanup()
+    }
+}
+
+const closetInitialize = (closet) => {
+    try {
+        if (globalThis.closetAfterShown) {
+            onShownHook.push(() => closetInit(closet))
+        }
+        else {
+            closetInit(closet)
+        }
+    }
+    catch {
+        closetInit(closet)
+    }
+}
+
 import(globalThis.ankiPlatform === 'desktop' ? '/__closet.js' : './__closet.js')
-    .then((closet) => {
-        try {
-            const times = closet.anki.init(closet, userLogic, '$$cardType', '$$tagsFull', '$$side')
-            console.info(`Closet executed in ${times.map(t => t.toFixed(3))}ms.`)
-        }
-        catch (error) {
-            console.error('An error occured while executing Closet:', error)
-        }
-        finally {
-            closet.browser.cleanup()
-        }
-    })
+    .then(closetInitialize)
     .catch((error) => console.error('An error occured while loading Closet:', error))

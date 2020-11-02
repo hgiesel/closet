fa1ecc0835627fa9d384c73d7b3d9a59f9dbadfd
Henrik Giesel <hengiesel@gmail.com>
2020-11-02 01:21:06 +0100

    Write __closet.css to media folder

--- anki/src/update.py
+++ anki/src/update.py
@@ -1,3 +1,4 @@
+from typing import Callable
 from pathlib import Path
 from os.path import dirname, realpath
 from os import remove
@@ -6,15 +7,20 @@ from glob import glob
 from aqt import mw
 
 
-def get_closet_source() -> str:
-    filepath = Path(dirname(realpath(__file__)), '..', 'web', 'closet.js')
+def get_source(source_name: str) -> Callable[[], str]:
+    def inner() -> str:
+        filepath = Path(dirname(realpath(__file__)), '..', 'web', source_name)
 
-    with open(filepath, mode='r', encoding='utf-8') as file:
-        return file.read().strip()
+        with open(filepath, mode='r', encoding='utf-8') as file:
+            return file.read().strip()
+
+    return inner
+
+get_closet_js = get_source('closet.js')
+get_closet_css = get_source('closet.css')
 
 # For some reason, esm modules with single leading underscore do not work
-closet_filename = '__closet.js'
-closet_file_regex = '__closet*.js'
+closet_file_regex = '__closet*.*'
 
 def update_closet() -> None:
     if not (basepath := mw.col.media.dir()):
@@ -24,4 +30,5 @@ def update_closet() -> None:
     for file in glob(str(Path(basepath, closet_file_regex))):
         remove(file)
 
-    mw.col.media.write_data(closet_filename, get_closet_source().encode())
+    mw.col.media.write_data('__closet.js', get_closet_js().encode())
+    mw.col.media.write_data('__closet.css', get_closet_css().encode())

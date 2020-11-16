80eb9c9333c639beda2bf04d3767da393e6006a1
Henrik Giesel <hengiesel@gmail.com>
2020-11-16 13:19:26 +0100

    Update qaNodes mechanism for Anki
    * only consider javascript document as fragment splitting

--- src/anki/qaNodes.ts
+++ src/anki/qaNodes.ts
@@ -1,6 +1,28 @@
 import type { ChildNodeSpan } from '../browser'
 import { interspliceChildNodes } from '../browser'
 
+const isElement = (tag: ChildNode): tag is Element => (
+    tag.nodeType === Node.ELEMENT_NODE
+)
+
+const isStyleElement = (tag: Element): tag is HTMLStyleElement => (
+    tag.tagName === 'STYLE'
+)
+
+const isScriptElement = (tag: Element): tag is HTMLScriptElement => (
+    tag.tagName === 'SCRIPT'
+)
+
+const isJavaScriptElement = (tag: Element): tag is HTMLScriptElement => (
+    isScriptElement(tag) &&
+    (tag.type.length === 0 || tag.type.endsWith('javascript'))
+)
+
+// Anki Asset Manager support
+const isAnkiAssetManagerElement = (tag: Element): tag is HTMLDivElement => (
+    tag.id === 'anki-am' 
+)
+
 export const getQaChildNodes = (): ChildNodeSpan[] | null => {
     if (!window.document) {
         return null
@@ -14,10 +36,12 @@ export const getQaChildNodes = (): ChildNodeSpan[] | null => {
 
     return interspliceChildNodes(qa, {
         type: 'predicate',
-        value: (v: any) => (
-            v.tagName !== 'STYLE' &&
-            v.tagName !== 'SCRIPT' &&
-            v.id !== 'anki-am' /* Asset Manager support */
+        value: (tag: ChildNode): boolean => !(
+            isElement(tag) && (
+                isStyleElement(tag) ||
+                isJavaScriptElement(tag) ||
+                isAnkiAssetManagerElement(tag)
+            )
         )
     })
 }

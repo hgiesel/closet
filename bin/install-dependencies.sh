e5b0c323afcfb01cf8df52682ad921934ce21563
Henrik Giesel <hengiesel@gmail.com>
2020-11-16 15:56:03 +0100

    Update build process for Ubuntu

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -3,12 +3,11 @@ declare DIR="$(cd "$(dirname "$0")/.." && pwd -P)"
 
 if [[ "$(uname)" == 'Darwin' ]]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
-  brew install imagemagick
-  brew install pyqt5
+  brew install imagemagick pyqt5
 
 elif [[ "$(uname)" == 'Linux' ]]; then
-  : # nothing yet
+  # Ubuntu
+  sudo apt-get install qtcreator pyqt5-dev-tools
 
 else
   echo 'Unknown plattform'

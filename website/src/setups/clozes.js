f63fc16b9f25dafafe9f570276c5d435153b006e
Henrik Giesel <hengiesel@gmail.com>
2021-02-20 18:28:30 +0100

    Start an experiment, to run Closet on docusaurus website

--- website/src/setups/clozes.js
+++ website/src/setups/clozes.js
@@ -2,5 +2,9 @@ export const title = "Clozes default";
 export const functionName = "clozes";
 
 function clozes(closet, filterManager, preset, memory) {
+    filterManager.install(closet.flashcard.recipes.cloze({
+        tagname: 'c',
+    }))
+
 
 }

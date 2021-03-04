cb3e1443be153b94657a80a1eaf20aae4a3554f1
Henrik Giesel <hengiesel@gmail.com>
2021-03-04 23:25:45 +0100

    Have a working CodeMirror on website

--- website/src/components/CodeEditor/index.tsx
+++ website/src/components/CodeEditor/index.tsx
@@ -1,2 +1,19 @@
+import React from "react"
+
 import { UnControlled as CodeMirror } from 'react-codemirror2'
 
+import "codemirror/lib/codemirror.css"
+
+const CodeEditor = () => {
+
+  return (
+    <CodeMirror
+      value={"<h1>I ♥ react-codemirror2</h1>"}
+      onChange={(editor, data, value) => {
+      }}
+    />
+  )
+}
+
+
+export default CodeEditor;

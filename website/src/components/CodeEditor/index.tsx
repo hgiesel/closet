38b1912b242c8f2c2bc7ceef37cf2a0cd95a3b9f
Henrik Giesel <hengiesel@gmail.com>
2021-03-05 00:48:26 +0100

    Add some highlights for CodeMirror

--- website/src/components/CodeEditor/index.tsx
+++ website/src/components/CodeEditor/index.tsx
@@ -3,17 +3,18 @@ import React from "react"
 import { UnControlled as CodeMirror } from 'react-codemirror2'
 
 import "codemirror/lib/codemirror.css"
+import "./styles.css"
 
 const CodeEditor = () => {
 
   return (
     <CodeMirror
-      value={"<h1>I ♥ react-codemirror2</h1>"}
+      value={"...your content here..."}
       onChange={(editor, data, value) => {
+        console.log(editor, data, value)
       }}
     />
   )
 }
-
 
 export default CodeEditor;

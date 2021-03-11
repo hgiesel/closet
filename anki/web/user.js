cab1a938cb695fec90573d523827fe3ba578f622
Henrik Giesel <hengiesel@gmail.com>
2021-03-11 14:02:33 +0100

    Update usage of closet library in anki app

--- anki/web/user.js
+++ anki/web/user.js
@@ -14,10 +14,10 @@ var getAnkiPrefix = () => globalThis.ankiPlatform === 'desktop'
     : '.'
 
 var closetPromise = import(`${getAnkiPrefix()}/__closet-$$version.js`)
-    .then(
-        closet => closet.template.anki.initialize(closet, closetUserLogic, '$$cardType', '$$tagsFull', '$$side'),
-        error => console.log('An error occured while loading Closet:', error),
-    ).catch(error => console.log('An error occured while executing Closet:', error))
+closetPromise.then(
+    ({ closet }) => closet.template.anki.initialize(closet, closetUserLogic, '$$cardType', '$$tagsFull', '$$side'),
+    error => console.log('An error occured while loading Closet:', error),
+).catch(error => console.log('An error occured while executing Closet:', error))
 
 if (globalThis.onUpdateHook) {
     onUpdateHook.push(() => closetPromise)

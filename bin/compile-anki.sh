5549f71c362cf263238195cef6c8497be0591187
Matthias Metelka <62722460+kleinerpirat@users.noreply.github.com>
2022-07-26 10:04:24 +0200

    Create qt5 and qt6 directories with build script

--- bin/compile-anki.sh
+++ bin/compile-anki.sh
@@ -1,5 +1,7 @@
 DIR="$(cd "$(dirname "$0")/../anki" && pwd -P)"
 
+mkdir -p "$DIR/gui/forms/qt5" "$DIR/gui/forms/qt6"
+
 for filename in "$DIR/designer/"*'.ui'; do
   pyuic5 "$filename" > "$DIR/gui/forms/qt5/$(basename ${filename%.*})_ui.py"
   pyuic6 "$filename" > "$DIR/gui/forms/qt6/$(basename ${filename%.*})_ui.py"

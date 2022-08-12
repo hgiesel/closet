769dc2fceb74823d819f175410da57cdee1fb025
Henrik Giesel <hengiesel@gmail.com>
2022-08-12 10:17:15 +0200

    Run pyuic directly over python executable

--- bin/compile-anki.sh
+++ bin/compile-anki.sh
@@ -3,8 +3,8 @@ DIR="$(cd "$(dirname "$0")/../anki" && pwd -P)"
 mkdir -p "$DIR/gui/forms/qt5" "$DIR/gui/forms/qt6"
 
 for filename in "$DIR/designer/"*'.ui'; do
-  pyuic5 "$filename" > "$DIR/gui/forms/qt5/$(basename ${filename%.*})_ui.py"
-  pyuic6 "$filename" > "$DIR/gui/forms/qt6/$(basename ${filename%.*})_ui.py"
+  python -m PyQt5.uic.pyuic "$filename" > "$DIR/gui/forms/qt5/$(basename ${filename%.*})_ui.py"
+  python -m PyQt6.uic.pyuic "$filename" > "$DIR/gui/forms/qt6/$(basename ${filename%.*})_ui.py"
 done
 
 echo 'Was successfully compiled!'
